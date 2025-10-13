ce5ee681f4c8929ac018d4d95825369965dd64fb
Aleksandr <a8csan@yandex.ru>
2025-10-13 15:49:10 +0300

    Добавил аналитическое решение системы

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@
 #include <fstream>
 #include <functional>
 
-#define EPS 1e-5
+#define EPS 1e-6
 // коэффициенты
 #define M 1
 double Kabs[2] = {1.029, 2.481};
@@ -36,6 +36,109 @@ std::vector<double> System(double t, std::vector<double>& y)
     dy_dt[5] = K31[M] * y[1] - K32[M] * y[5] + K33[M];
 
     return dy_dt;
+}
+
+// метод Гаусса
+std::vector<double> gauss(std::vector<std::vector<double>>& A, std::vector<double>& b) {
+    size_t row_size = A.size();
+    size_t col_size = A.back().size();
+    // Прямой ход Гаусса
+    double pivot = 0.0;
+    for (size_t i = 0; i < row_size; i++) {
+        for (size_t j = i + 1; j < col_size; j++) {
+            if (std::abs(A.at(j).at(i)) < EPS) {
+                continue;
+            }
+            pivot = A.at(j).at(i) / A.at(i).at(i);
+            b.at(j) -= pivot * b.at(i);
+            for (size_t k = 0; k < row_size; k++) {
+                A.at(j).at(k) -= pivot * A.at(i).at(k);
+            }
+        }
+    }
+
+    // Обратный ход Гаусса
+    std::vector<double> x(row_size);
+    for (int i = row_size - 1; i >= 0; i--) {
+        x.at(i) = b.at(i);
+        for (size_t j = i + 1; j < row_size; j++) {
+            x.at(i) -= x.at(j) * A.at(i).at(j);
+        }
+        if (A.at(i).at(i) == 0)
+            return {-1,-1,-1,-1};
+        x.at(i) /= A.at(i).at(i);
+    }
+
+    return x;
+}
+
+// аналитическое решение системы
+std::vector<double> analytical_solution(double t, std::vector<std::vector<double>> initial)
+{
+    // Initial - начальные условия;   задаются как initial = {t, c}, где yi(t) = c
+    // Решение системы
+    std::vector<double> y;
+    y.resize(6);
+
+    // Собственные числа лямбда |A-labmdaE| = 0
+    double lambda_1 = Kabs[M];
+    double lambda_2 = ((-1)*(K1[M]+Kel[M]+K2[M]) + (sqrt((K1[M]+Kel[M]+K2[M])*(K1[M]+Kel[M]+K2[M])-4*K2[M]*Kel[M])))/2;
+    double lambda_3 = ((-1)*(K1[M]+Kel[M]+K2[M]) - (sqrt((K1[M]+Kel[M]+K2[M])*(K1[M]+Kel[M]+K2[M])-4*K2[M]*Kel[M])))/2;
+
+    //Собственные векторы для лямбд |A-labmdaE| = 0
+    std::vector<double> e1;
+    std::vector<double> e2;
+    std::vector<double> e3;
+
+    std::vector<std::vector<double>> A1 = {
+        {-Kabs[M]-lambda_1,         0,                           0},
+        {Kabs[M],                   -(K1[M]+Kel[M])-lambda_1,    K2[M]},
+        {0,                         K1[M],                      -K2[M]-lambda_1}
+    };
+    std::vector<std::vector<double>> A2 = {
+        {-Kabs[M]-lambda_2,  0,                  0},
+        {Kabs[M],   -(K1[M]+Kel[M])-lambda_2,    K2[M]},
+        {0,         K1[M],              -K2[M]-lambda_2}
+    };
+    std::vector<std::vector<double>> A3 = {
+        {-Kabs[M]-lambda_3,  0,                  0},
+        {Kabs[M],   -(K1[M]+Kel[M])-lambda_3,    K2[M]},
+        {0,         K1[M],              -K2[M]-lambda_3}
+    };
+
+    std::vector<double> b = {0,0,0};
+
+    e1 = gauss(A1,b);
+    e2 = gauss(A3,b);
+    e3 = gauss(A3,b);
+
+    // вычисление С из начальных условий RC = I для у
+    std::vector<double> C;
+
+    std::vector<std::vector<double>> R = {
+        {exp(lambda_1*initial[0][0])*e1[0],  exp(lambda_2*initial[0][0])*e2[0], exp(lambda_3*initial[0][0])*e3[0]},
+        {exp(lambda_1*initial[1][0])*e1[1],exp(lambda_2*initial[1][0])*e2[1],exp(lambda_3*initial[1][0])*e3[1]},
+        {exp(lambda_1*initial[2][0])*e1[2],exp(lambda_2*initial[2][0])*e2[2],exp(lambda_3*initial[2][0])*e3[2]}
+    };
+
+    std::vector<double> I = {initial[0][1], initial[1][1],initial[2][1]};
+
+    C = gauss(R,I);
+
+    for (int i=0; i<3;i++)
+        y[i] = C[0]*exp(lambda_1*t)*e1[i] + C[1]*exp(lambda_2*t)*e2[i] + C[2]*exp(lambda_3*t)*e3[i];
+
+    // вычисление С из начальных условий RC = I для x
+    double C4 = (initial[3][1] - (K13[M]-K11[M]*y[1])/(K12[M]) * exp(K12[M]*initial[3][0]));
+    double C5 = (initial[4][1] - (K23[M]-K21[M]*y[1])/(K22[M]) * exp(K22[M]*initial[4][0]));
+    double C6 = (initial[5][1] - (K33[M]-K31[M]*y[1])/(K32[M]) * exp(K32[M]*initial[5][0]));
+
+    y[3] = C4/exp(K12[M]*t) + (K13[M]-K11[M]*y[1])/(K12[M]);
+    y[4] = C5/exp(K22[M]*t) + (K23[M]-K21[M]*y[1])/(K22[M]);
+    y[5] = C6/exp(K32[M]*t) + (K33[M]-K31[M]*y[1])/(K32[M]);
+
+    return y;
+
 }
 
 int main()
@@ -73,5 +176,26 @@ int main()
     y = { 0.0, 0.0, 0.0, 0.0, 0.0, 0.0};
     Milne_Simpson(System, y, t0, t1, h, &csv2);
 
+    // начальные условия
+    std::vector<std::vector<double>> initial = {
+            {0,0},
+            {0,0},
+            {0,0},
+            {0,0},
+            {0,0},
+            {0,0},
+    };
+
+    char* name4 = (char*)"Analytical_solution.csv";
+    std::ofstream csv4;
+    csv4.open(name4);
+
+    for (double t=0.0; t<t1+h; t+=h)
+    {
+        std::vector<double> ya = analytical_solution(t, initial);
+        for (int i=0; i<5; i++)
+            csv4 << ya[i] << ", " ;
+        csv4 << ya[5] << std::endl;
+    }
     return 0;
 }
--- main.py
+++ main.py
@@ -5,6 +5,8 @@ import matplotlib.pyplot as plt
 result1 = np.empty(shape=(102, 6))
 result2 = np.empty(shape=(102, 6))
 result3 = np.empty(shape=(102, 6))
+result4 = np.empty(shape=(102, 6))
+
 with open('Build\Debug\RungeKutta.csv', newline='') as csvfile:
     reader = csv.reader(csvfile, delimiter=',')
     j = 0
@@ -35,15 +37,32 @@ with open('Build\Debug\Milne_Simpson.csv', newline='') as csvfile3:
             i += 1
         j += 1
 
+with open('Build\Debug\Analytical_solution.csv', newline='') as csvfile4:
+    reader = csv.reader(csvfile4, delimiter=',')
+    j = 0
+    for row in reader:
+        i = 0
+        for cell in row:
+            result4[j][i] = cell
+            i += 1
+        j += 1
+
 t = np.arange(0., 10.2, 0.1)
-plt.plot(t, result2[:, 3], 'r', label='X1_A-B')
-plt.plot(t, result2[:, 4], 'b', label='X2_A-B')
-plt.plot(t, result2[:, 5], 'g', label='X3_A-B')
-plt.plot(t, result1[:, 3], 'r', label='X1_R-K')
-plt.plot(t, result1[:, 4], 'b', label='X2_R-K')
-plt.plot(t, result1[:, 5], 'g', label='X3_R-K')
-plt.plot(t, result3[:, 3], 'r', label='X1_R-K')
-plt.plot(t, result3[:, 4], 'b', label='X2_R-K')
-plt.plot(t, result3[:, 5], 'g', label='X3_R-K')
+plt.plot(t, result1[:, 3], 'r', label='X1_RungeKutta')
+plt.plot(t, result1[:, 4], 'b', label='X2_RungeKutta')
+plt.plot(t, result1[:, 5], 'g', label='X3_RungeKutta')
+
+plt.plot(t, result2[:, 3], 'r', label='X1_Adams_Bashforth_Moulton')
+plt.plot(t, result2[:, 4], 'b', label='X2_Adams_Bashforth_Moulton')
+plt.plot(t, result2[:, 5], 'g', label='X3_Adams_Bashforth_Moulton')
+
+plt.plot(t, result3[:, 3], 'r', label='X1_Milne_Simpson')
+plt.plot(t, result3[:, 4], 'b', label='X2_Milne_Simpson')
+plt.plot(t, result3[:, 5], 'g', label='X3_Milne_Simpson')
+
+plt.plot(t, result4[:, 3], 'r', label='X1_Analytical')
+plt.plot(t, result4[:, 4], 'b', label='X2_Analytical')
+plt.plot(t, result4[:, 5], 'g', label='X3_Analytical')
+
 plt.legend()
 plt.show()
